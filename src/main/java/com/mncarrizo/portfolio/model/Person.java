a5fa45810d3162fd454d558177e066ab2f0b2069
marinanc <marinancarrizo@gmail.com>
2023-02-28 20:17:35 -0300

    Add packages and entity Person. on Model

---
+++ src/main/java/com/mncarrizo/portfolio/model/Person.java
@@ -0,0 +1,35 @@
+
+package com.mncarrizo.portfolio.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Marina
+ */
+
+@Getter @Setter
+@Entity
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String nombre;
+    
+    @NotNull
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String apellido;
+    
+    @Size(min = 1, max = 50, message = "No cumple con la longitud")
+    private String img;
+}
