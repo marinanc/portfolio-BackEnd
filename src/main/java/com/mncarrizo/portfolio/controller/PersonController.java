aa150e0214dbf2631681c8d781ebf0ce47f5ed00
marinanc <marinancarrizo@gmail.com>
2023-03-03 00:23:47 -0300

    Fix CORS localhost:4200

--- src/main/java/com/mncarrizo/portfolio/controller/PersonController.java
+++ src/main/java/com/mncarrizo/portfolio/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.mncarrizo.portfolio.model.Person;
 import com.mncarrizo.portfolio.service.IPersonService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonController {
     @Autowired IPersonService personService;
     
