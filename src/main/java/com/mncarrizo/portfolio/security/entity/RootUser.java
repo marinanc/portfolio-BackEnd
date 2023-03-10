522c6c618e33f74ee9a444cbcb25ca24402bf950
marinanc <marinancarrizo@gmail.com>
2023-03-10 00:24:51 -0300

    Add RootUser implements UserDetails

---
+++ src/main/java/com/mncarrizo/portfolio/security/entity/RootUser.java
@@ -0,0 +1,85 @@
+package com.mncarrizo.portfolio.security.entity;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ *
+ * @author Marina
+ */
+
+public class RootUser implements UserDetails{
+    private String name;
+    private String userName;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public RootUser(String name, String userName, String email, String password, Collection<? extends GrantedAuthority> authorities) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.authorities = authorities;
+    }
+    
+    public static RootUser build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                .collect(Collectors.toList());
+        
+        return new RootUser(
+                user.getName(), 
+                user.getUserName(), 
+                user.getEmail(),
+                user.getPassword(),
+                authorities);
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
