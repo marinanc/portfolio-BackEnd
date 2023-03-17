873d02de39f01930f96d010266bcd766ee5308f2
marinanc <marinancarrizo@gmail.com>
2023-03-17 03:30:39 -0300

    Fix ExperienceController
    Modified @Controller for @RestController

--- src/main/java/com/mncarrizo/portfolio/controller/EducationController.java
+++ src/main/java/com/mncarrizo/portfolio/controller/EducationController.java
@@ -1,9 +1,17 @@
 package com.mncarrizo.portfolio.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Marina
  */
+
+@RestController
+@RequestMapping("education")
+@CrossOrigin(origins = "http://localhost:4200")
 public class EducationController {
     
 }
