5184a932438a7ad61681e41a77dd09ba5b13d38c
marinanc <marinancarrizo@gmail.com>
2023-03-17 02:53:37 -0300

    Fix update and delete experience

--- src/main/java/com/mncarrizo/portfolio/controller/ExperienceController.java
+++ src/main/java/com/mncarrizo/portfolio/controller/ExperienceController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,7 +50,7 @@ public class ExperienceController {
         return new ResponseEntity(new Message("Experiencia agregada exitosamente"), HttpStatus.OK);
     }
     
-    @PutMapping("update/id")
+    @PutMapping("update/{id}")
     public ResponseEntity<?> update(@PathVariable("id") int id, @RequestBody dtoExperience dtoExp){
         if(!experienceService.existsById(id))
             return new ResponseEntity(new Message("El id no existe"), HttpStatus.BAD_REQUEST);
@@ -71,6 +72,7 @@ public class ExperienceController {
         return new ResponseEntity(new Message("Experiencia actualizada exitosamente"), HttpStatus.OK);
     }
     
+    @DeleteMapping("delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!experienceService.existsById(id))
             return new ResponseEntity(new Message("El ID no existe"), HttpStatus.BAD_REQUEST);
@@ -78,5 +80,13 @@ public class ExperienceController {
         experienceService.delete(id);
         
         return new ResponseEntity(new Message("Experiencia eliminada exitosamente"),HttpStatus.OK);
+    }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Experience> getById(@PathVariable("id") int id){
+        if(!experienceService.existsById(id))
+            return new ResponseEntity(new Message("no existe"), HttpStatus.NOT_FOUND);
+        Experience experiencie = experienceService.getOne(id).get();
+        return new ResponseEntity(experiencie, HttpStatus.OK);
     }
 }
