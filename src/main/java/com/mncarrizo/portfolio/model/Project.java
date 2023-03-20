684fdcccdead58d981e70af0ca9b5f72d9dbfb6c
marinanc <marinancarrizo@gmail.com>
2023-03-20 02:52:59 -0300

    Fix project entity

--- src/main/java/com/mncarrizo/portfolio/model/Project.java
+++ src/main/java/com/mncarrizo/portfolio/model/Project.java
@@ -1,10 +1,20 @@
 package com.mncarrizo.portfolio.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 /**
  *
  * @author Marina
  */
+
+@Entity
 public class Project {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String description;
